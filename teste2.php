23575852b248f0ced996015c1be9e17cb629c525
rodrigodeananias <rodrigo@alquati.com.br>
2016-09-29 11:21:19 -0300

    Rodrigo
    alterou o arquivo bla

---
+++ teste2.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+</head>
+<body>
+<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+<script type="text/javascript">
+jQuery(function($){
+	setTimeout(function(){
+		//alert('Huehuebr');
+		javascript:Journal.openPopup('20');
+	},5000);
+});
+</script>
+</body>
+</html>
